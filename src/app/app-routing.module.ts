bf0a9029fe25a867d9b826e8e92739d2de55dd2e
Walid Sboui <walid.sboui@insat.ucar.tn>
2024-01-27 01:57:35 +0100

    add routing + generating cart/header/home/notFound404/products/wishlist components  + add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ProductsComponent } from './products/products.component';
+import {HomeComponent} from "./home/home.component";
+import { NotFound404Component }from "./not-found404/not-found404.component"
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'wishlist', component: WishlistComponent },
+  { path: '', component: HomeComponent , pathMatch: 'full'},
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: '**', component: NotFound404Component  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { NotFound404Component } from './not-found404/not-found404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    WishlistComponent,
+    CartComponent,
+    HomeComponent,
+    NotFound404Component
   ],
   imports: [
     BrowserModule,
