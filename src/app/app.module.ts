1d02da9f4c6627fe566a0fa4af5a51f85858f526
lakhdher <noureddine.bennejma@insat.u-carthage.tn>
2024-01-27 12:50:23 +0100

    :sparkles: feat : introduce accordion, and enhance project architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './pages/products/products.component';
-import { ProductCardComponent } from './pages/products/product-card/product-card.component';
+import { ProductCardComponent } from './pages/products/UI/product-card/product-card.component';
+import { SidebarFilterComponent } from './pages/products/UI/sidebar-filter/sidebar-filter.component';
 
 import {
   RiAncientGateFill,
   RiHome2Fill,
   RemixIconModule,
   RiHeart3Line,
+  RiShoppingCart2Fill,
 } from 'angular-remix-icon';
+import { AccordionComponent } from './pages/products/UI/accordion/accordion.component';
+import { AccordionItemComponent } from './pages/products/UI/accordion-item/accordion-item.component';
+import { ButtonComponent } from './pages/products/UI/button/button.component';
 
 const icons = {
   RiAncientGateFill,
   RiHome2Fill,
   RiHeart3Line,
+  RiShoppingCart2Fill
 };
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, ProductCardComponent],
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductCardComponent,
+    SidebarFilterComponent,
+    AccordionComponent,
+    AccordionItemComponent,
+    ButtonComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, RemixIconModule.configure(icons)],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/products/UI/button/button.component.ts
@@ -0,0 +1,35 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+} from '@angular/core';
+
+@Component({
+  selector: 'button[wfh]',
+  template: ` <ng-content></ng-content> `,
+  styleUrls: ['./button.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class ButtonComponent {
+  @Input()
+  class = '';
+
+  @Input()
+  variant = 'primary';
+
+  @Input()
+  size = 'base';
+
+  @HostBinding('class')
+  get buttonClasses() {
+    const baseClasses = new Set([
+      'button',
+      this.variant ?? 'primary',
+      this.size,
+    ]);
+    this.class.split(' ').forEach((c) => baseClasses.add(c));
+    return Array.from(baseClasses).join(' ');
+  }
+}
