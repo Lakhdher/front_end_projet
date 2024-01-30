bc1d3e771659125f0cc0f49c646dfa86d7afe779
lakhdher <noureddine.bennejma@insat.u-carthage.tn>
2024-01-31 00:19:05 +0100

    :lipstick: fix : fix side filter UI and colors filter

--- src/app/pages/products/mock_data/colors.ts
+++ src/app/pages/products/mock_data/colors.ts
@@ -1,30 +1,36 @@
 export const COLORS = [
   {
+    index: 0,
     label: 'Black',
     value: 'black',
     color: '#000',
   },
   {
+    index: 1,
     label: 'White',
     value: 'white',
     color: '#fff',
   },
   {
+    index: 2,
     label: 'Red',
     value: 'red',
     color: '#ce1212',
   },
   {
+    index: 3,
     label: 'Green',
     value: 'green',
     color: '#105610',
   },
   {
+    index: 4,
     label: 'Blue',
     value: 'blue',
     color: '#2251b0',
   },
   {
+    index: 5,
     label: 'Brown',
     value: 'brown',
     color: '#5b3e3a',
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { PRODUCTS } from './mock_data/products';
 
-
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
-openQuickView(arg0: any) {
-throw new Error('Method not implemented.');
-}
+  applyFilter($event: any) {
+    throw new Error('Method not implemented.');
+  }
+  openQuickView(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
   clearSearch() {
     throw new Error('Method not implemented.');
   }
