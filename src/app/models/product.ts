f11aa5a561cc7e130cdef7f2e0120c6c2982c1c5
Walid Sboui <walid.sboui@insat.ucar.tn>
2024-02-01 14:37:50 +0100

    product Model Change

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -3,16 +3,27 @@ export class Product {
     public id: number,
     public name: string,
     public price: number,
+    public originalPrice: number,
     public description: string,
-    public imageUrl: string | undefined,
+    public images: string[] | undefined,
     public stock: number | undefined,
     public category: string | undefined,
     public brand: string | undefined,
     public rating: number | undefined,
-    public discount: number | undefined,
-    public color: string| undefined,
+    public colors: string [] | undefined,
+    public specs: {
+      type: string | null;
+      numberOfKeys: number;
+      keyCaps: string;
+      illumination: string;
+      buttons: number;
+      resolution: string;
+      headSupport: string;
+      size: string;
+      upholstery: string;
+      material: string;
+      connectivity: string;
+    }
   ) {
   }
-
-
 }
