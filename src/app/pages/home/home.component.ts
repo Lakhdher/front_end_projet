4c13242a6e1a907f25ec7adc220964503629cb06
lakhdher <noureddine.bennejma@insat.u-carthage.tn>
2024-02-01 23:28:55 +0100

    :poop: fix : add quick view to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {AsyncPipe, NgClass, NgForOf, NgStyle} from "@angular/common";
-import {MatButtonModule} from '@angular/material/button';
-import {ProductsService} from "../../services/products.service";
-import {Observable} from "rxjs";
+import { Component, ViewChild } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { AsyncPipe, NgClass, NgForOf, NgStyle } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { ProductsService } from '../../services/products.service';
+import { Observable } from 'rxjs';
+import { OverlayService } from 'src/app/services/overlay.service';
+import { Product } from 'src/app/models/product';
+import { ProductQuickViewComponent } from '../products/products-quick-view/products-quick-view.component';
+import { AppModule } from 'src/app/app.module';
 
 export interface Tile {
   cols: number;
@@ -23,18 +27,31 @@ export interface Tile {
     NgClass,
     MatButtonModule,
     NgStyle,
-    AsyncPipe
-  ]
+    AsyncPipe,
+  ],
 })
 export class HomeComponent {
-  products$: Observable<any[]>;
+  @ViewChild(ProductQuickViewComponent, { static: true })
+  productQuickViewRef?: ProductQuickViewComponent;
+  activeProduct$: Observable<any> = this.overlay.activeProduct$;
 
+  products$: Observable<any[]>;
   constructor(
     private productsService: ProductsService,
+    private overlay: OverlayService
   ) {
-    this.products$ = this.productsService.getProducts(4,3.75);
-
+    this.products$ = this.productsService.getProducts(4, 3.75);
+    overlay.clickedOutside$.subscribe(() => {
+      this.productQuickViewRef?.close();
+    });
   }
 
+  selectProduct(product: Product) {
+    this.productQuickViewRef?.open();
+    this.overlay.activeProductSubject.next(product);
+  }
 
+  addToCart($event: any) {
+    throw new Error('Method not implemented.');
+  }
 }
